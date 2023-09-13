f124cff99467391782f8e4cddf14f0edb1796b85
Amit Tripathi <tripathi.ce@gmail.com>
2023-09-13 09:04:06 +0000

    added new service point for inference

--- counter/entrypoints/webapp.py
+++ counter/entrypoints/webapp.py
@@ -18,6 +18,15 @@ def object_detection():
     count_response = count_action.execute(image, threshold)
     return jsonify(count_response)
 
+@app.route('/new-service-endpoint', methods=['POST'])
+def predict_objects():
+    uploaded_file = request.files['file']
+    threshold = float(request.form.get('threshold', 0.5))
+    image = BytesIO()
+    uploaded_file.save(image)
+    count_response = count_action.execute(image, threshold)
+    return jsonify(count_response)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
